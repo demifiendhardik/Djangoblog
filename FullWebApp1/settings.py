936f56af6991bd966fabebcd73b1210498771cdd
Hardik Sharma <demifiendhardik@gmail.com>
2019-06-26 22:36:52 +0530

    Updated MIDDLEWARE

--- FullWebApp1/settings.py
+++ FullWebApp1/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'WM.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'WM.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'FullWebApp1.urls'
